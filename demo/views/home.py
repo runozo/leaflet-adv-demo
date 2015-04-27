ed8668ee7cb4d0d00d7cbedb6c639001b1f4ebff
Cesare Ghirelli <cghirelli@arpa.emr.it>
2015-04-27 23:56:29 +0200

    Added save marker logic

--- demo/views/home.py
+++ demo/views/home.py
@@ -1,9 +1,9 @@
 # -*- coding: utf-8 -*-
 # from ghiro import app
 from jinja2 import TemplateNotFound
-from flask import Blueprint, render_template, abort, current_app
+from flask import Blueprint, render_template, abort, current_app, request, jsonify
 from datetime import datetime
-from demo.database import engine
+# from demo.database import engine
 
 mod = Blueprint('home', __name__)
 
@@ -14,3 +14,12 @@ def index():
         return render_template('index.html')
     except TemplateNotFound:
         abort(404)
+
+
+@mod.route('/newmarker', methods=['post'])
+def newmarker():
+    try:
+        print(request.form)
+        return jsonify({'message': 'ok'})
+    except Exception, e:
+        return jsonify({'message': 'error', 'value': str(e)})
